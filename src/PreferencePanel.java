d54d727c499d9b16abf60b44ea757594fb0b818c
Tetsurou-macpro <n.teturou@mac.com>
2014-08-13 15:52:11 +0900

    Succeeded to retrieve calendar using OAuth

---
+++ src/PreferencePanel.java
@@ -0,0 +1,22 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+
+public class PreferencePanel extends JFrame implements ActionListener{
+	PreferencePanel(){
+		
+	}
+	
+	public void showPanel(){
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
